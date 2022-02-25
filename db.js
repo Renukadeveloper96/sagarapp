b863163fd4d2669420cd84d28857e800f593f564
Renukadeveloper96 <renukamaradagi@gmail.com>
2022-02-25 22:51:27 +0530

    sagarapp

---
+++ db.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+/*mongoose.connect(
+  "mongodb+srv://m001-student:vedantu123@sandbox.cou6h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);*/
+/*mongoose.connect(
+  "mongodb+srv://zomato:zomato77@cluster0.nbutl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+)*/
+mongoose.connect("mongodb+srv://renuka:renuka23@cluster0.nbutl.mongodb.net/loginregister?retryWrites=true&w=majority");
